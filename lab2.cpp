140920092b18eb70b4bc14935833d6f5e802d15f
molidex <kakakakaka2018@mail.ru>
2020-04-24 18:17:26 +0300

    Implemented fully class Polynomial. Added simple interface for type double.

--- lab2.cpp
+++ lab2.cpp
@@ -5,6 +5,8 @@
 #include "sequence.h"
 #include "list_sequence.h"
 
+constexpr auto InsufficientSizeListException = "One of polynomial has insufficient size";
+
 //node_dptr - node double pointer
 
 
@@ -35,6 +37,8 @@ private:
 
 public:
 
+    Polynomial() = default;
+	
     Polynomial(const int size)
     {
         T* data = new T[size];
@@ -130,13 +134,66 @@ public:
         return  answer;
 	}
 
+    Polynomial<T> multiply(const Polynomial<T>& poly)
+    {
+        if (this->size == 0 || poly.size == 0) throw std::logic_error(InsufficientSizeListException);
+
+        const int result_size = this->size + poly.size - 1;
+        Polynomial<T> result_poly(result_size);
+
+        for (int i = 0; i < this->size; ++i)
+        {
+            for (int j = 0; j < poly.size; ++j)
+            {
+                (*result_poly.coefs)[i + j] += (*this->coefs)[i] * (*poly.coefs)[j];
+            }
+        }
+        return  result_poly;
+
+    }
+
+    Polynomial<T> composition(Polynomial<T>& poly)
+    {
+        if (this->size <= 1 || poly.size <= 1) throw std::logic_error(InsufficientSizeListException);
+
+        std::cout << "f:";
+        this->show();
+
+        std::cout << "g:";
+        poly.show();
+		
+		
+        const int result_size = (poly.size - 1) * (this->size - 1) + 1;
+        Polynomial<T> result_poly(result_size);
+        Polynomial<T> temp_poly(result_size);
+        std::cout << "\n"  "RESULT SIZE: " << result_size << "\n";
+
+		for (int i = 0; i < this->size - 1; ++i)
+		{
+            temp_poly = poly;
+
+            for (int j = 0; j < this->size - i - 2; ++j) temp_poly = temp_poly.multiply(poly);
+
+            temp_poly *= (*this->coefs)[this->size - i - 1];
+
+            std::cout << (*this->coefs)[this->size - i - 1] << "*g^" << this->size - i - 1 << ":" << "\n";
+            temp_poly.show();
+			
+            result_poly = result_poly + temp_poly;
+		}
+		
+        (*result_poly.coefs)[0] += (*this->coefs)[0];
+
+        return result_poly;
+    }
+
 	void add_new_max(const T coef)
     {
         this->coefs->append(coef);
         this->size = this->coefs->get_size();
     }
 	
-    void add(const T coef, const int exponent)
+    void set(const T coef, const int exponent)
     {
         if (exponent < 0)
         {
@@ -174,14 +231,9 @@ public:
                 if ((*this->coefs)[i] != T(0)) std::cout << " + " << (*this->coefs)[i] << "*x^" << i;
             }
         }
-    }
-    void composition(const Polynomial<T> poly)
-	{
-        std::cout << "<composition>";
-	}
 
-	
-	
+        std::cout << "\n";
+    }
 };
 
 
@@ -190,7 +242,151 @@ public:
 
 int main()
 {
-   
-    return  0;
+    //Интерфейс сделан быстро, на коленках, и работает он только для одного типа double
+	
+    double* data;
+    int size_a = 0;
+    int size_b = 0;
+    int INPUT = 0;
+    double temp;
+    double k;
+    Polynomial<double> a(0);
+    Polynomial<double> b(0);
+    Polynomial<double> r(0);
+	
+	while (true)
+	{
+        std::cout << "\n";
+        printf("%s\n",
+            "1. Create a Polynomial A\n"
+            "2. Create a Polynomial B\n"
+            "3. Create a scalar k\n"
+            "4. R = A+B\n"
+            "5. R = A*B\n"
+            "6. R = n*A\n"
+            "7. m = n*B\n"
+            "8. R = A(k)\n"
+            "9. R = B(k)\n"
+            "10. R = A(B)\n"
+            "11. R = B(A)\n"
+            "12. exit\n");
+		
+        std::cin >> INPUT;
+
+        switch (INPUT)
+        {
+        case (1):
+
+            std::cout << "Size of A: ";
+            std::cin >> size_a;
+            data = new double[size_a];
+ 
+            std::cout << "\nCoefficients of the Polynomial (from C0 to Cn)\n";
+
+            for (int i = 0; i < size_a; ++i) std::cin >> data[i];
+        	
+            a = Polynomial<double>(data, size_a);
+            a.show();
+        	
+            delete[] data;
+
+        	break;
+
+
+        case (2):
+
+            std::cout << "Size of B: ";
+            std::cin >> size_b;
+            data = new double[size_b];
+
+            std::cout << "\nCoefficients of the Polynomial (from C0 to Cn)\n";
+
+            for (int i = 0; i < size_b; ++i) std::cin >> data[i];
+
+            b = Polynomial<double>(data, size_b);
+            b.show();
+
+            delete[] data;
+        	
+            break;
+
+        case (3):
+        	
+            std::cin >> k;
+        	
+            break;
+
+        case (4):
+
+            r = a + b;
+            r.show();
+
+            break;
+
+        case(5):
+
+            r = a.multiply(b);
+            r.show();
+        	
+            break;
+
+        case (6):
+
+            a *= k;
+            r = a;
+            r.show();
+            a *= 1 / k;
+
+            break;
+
+        case (7):
+
+            b *= k;
+            r = b;
+            r.show();
+            b *= 1 / k;
+
+            break;
+
+        case (8):
+
+            std::cout << a.substitute(k);
+
+            break;
+
+        case (9):
+
+            std::cout << b.substitute(k);
+
+            break;
+
+
+        case (10):
+
+            r = a.composition(b);
+            r.show();
+
+            break;
+
+        case (11):
+
+            r = b.composition(a);
+            r.show();
+
+            break;
+
+        case (12):
+
+            return 1337;
+
+        default:
+            std::cout << "\nINCORRECT INPUT!\n";
+        }
+    }
 }
+
+
+
+	
+
 
